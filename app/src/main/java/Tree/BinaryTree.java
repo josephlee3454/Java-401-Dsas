f82420ed81ae6a8e88a12be040e805a3e689ac6e
Joe <joseph.lee3454@gmail.com>
2021-04-13 08:52:26 -0700

    added the findMax

--- app/src/main/java/Tree/BinaryTree.java
+++ app/src/main/java/Tree/BinaryTree.java
@@ -2,66 +2,85 @@ package Tree;
 import java.util.*;
 public class BinaryTree {
     public Node root = null;
-    public ArrayList<Integer> numbers = new ArrayList<>();
+//    public ArrayList<Integer> numbers = new ArrayList<>();
 
 
     public BinaryTree(Node root){
         this.root = root;
     }
 
-    public ArrayList<Integer> preOrder(){
-        if (root == null){
-            return numbers;
+//    public ArrayList<Integer> preOrder(){
+//        if (root == null){
+//            return numbers;
+//        }
+//        _preOrderTraversal(root);
+//        return numbers;
+//    }
+//
+//    public void _preOrderTraversal(Node newNode){
+//        numbers.add(newNode.value);
+//        if(newNode.left != null){
+//            _preOrderTraversal(newNode.left);
+//        }
+//        if(newNode.right != null){
+//            _preOrderTraversal(newNode.right);
+//        }
+//    }
+//
+//    public ArrayList<Integer> inOrder(){
+//        if (root == null){
+//            return numbers;
+//        }
+//        _preOrderTraversal(root);
+//        return numbers;
+//    }
+//
+//    public void _inOrderTraversal(Node newNode){
+//        if(newNode.left != null){
+//            _inOrderTraversal(newNode.left);
+//        }
+//        numbers.add(newNode.value);
+//        if(newNode.right != null){
+//            _inOrderTraversal(newNode.right);
+//        }
+//    }
+//
+//    public ArrayList<Integer> postOrder(){
+//        if(root == null){
+//            return numbers;
+//        }
+//        _postOrderTraversal(root);
+//        return numbers;
+//    }
+//
+//    public void _postOrderTraversal(Node newNode){
+//        if(newNode.left != null){
+//            _postOrderTraversal(newNode.left);
+//        }
+//        if(newNode.right != null){
+//            _postOrderTraversal(newNode.right);
+//        }
+//        numbers.add(newNode.value);
+//    }
+
+
+    public int findMax(Node node)
+    {
+        if (node == null){
+         return 0;
         }
-        _preOrderTraversal(root);
-        return numbers;
-    }
-    
-    public void _preOrderTraversal(Node newNode){
-        numbers.add(newNode.value);
-        if(newNode.left != null){
-            _preOrderTraversal(newNode.left);
-        }
-        if(newNode.right != null){
-            _preOrderTraversal(newNode.right);
-        }
+
+        int maxCheck = node.value;
+        int leftCheck = findMax(node.left);
+        int rightCheck = findMax(node.right);
+
+        if (leftCheck > maxCheck)
+            maxCheck = leftCheck;
+        if (rightCheck > maxCheck)
+            maxCheck = rightCheck;
+        return maxCheck;
     }
 
-    public ArrayList<Integer> inOrder(){
-        if (root == null){
-            return numbers;
-        }
-        _preOrderTraversal(root);
-        return numbers;
-    }
-
-    public void _inOrderTraversal(Node newNode){
-        if(newNode.left != null){
-            _inOrderTraversal(newNode.left);
-        }
-        numbers.add(newNode.value);
-        if(newNode.right != null){
-            _inOrderTraversal(newNode.right);
-        }
-    }
-
-    public ArrayList<Integer> postOrder(){
-        if(root == null){
-            return numbers;
-        }
-        _postOrderTraversal(root);
-        return numbers;
-    }
-
-    public void _postOrderTraversal(Node newNode){
-        if(newNode.left != null){
-            _postOrderTraversal(newNode.left);
-        }
-        if(newNode.right != null){
-            _postOrderTraversal(newNode.right);
-        }
-        numbers.add(newNode.value);
-    }
 
 
 }
--- app/src/main/java/Tree/Node.java
+++ app/src/main/java/Tree/Node.java
@@ -4,6 +4,7 @@ public class Node {
     public int value;
     public Node left;
     public Node right;
+//    public int data;
 
     public Node(int value){
         this.left = null;
